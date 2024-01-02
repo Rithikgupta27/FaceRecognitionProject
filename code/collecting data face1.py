f37f08e9d97a5bfe56dfc2dc3c7fdfcd19a4b95d
Rithik Gupta <117571318+Rithikgupta27@users.noreply.github.com>
2024-01-02 23:41:36 +0530

    Update collecting data face1.py

--- code/collecting data face1.py
+++ code/collecting data face1.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-face_classifier = cv2.CascadeClassifier('C:\\Users\RohIL gupta\\anaconda3\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml')
+face_classifier = cv2.CascadeClassifier('C:\\Users\Rithik gupta\\anaconda3\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml')
 
 def face_extractor(img):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -21,7 +21,7 @@ while True:
         face = cv2.resize(face_extractor(frame),(200,200))
         face = cv2.cvtColor(face,cv2.COLOR_BGR2GRAY)
 # my face file position
-        file_name_path = 'C:\\Users\\RohIL gupta\\anaconda3\\images\\img1'+str(count)+'.jpg'
+        file_name_path = 'C:\\Users\\Rithik gupta\\anaconda3\\images\\img1'+str(count)+'.jpg'
         cv2.imwrite(file_name_path,face)
 
         cv2.putText(face,str(count),(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
